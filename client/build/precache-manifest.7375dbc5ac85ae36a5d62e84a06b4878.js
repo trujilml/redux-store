22d31f4989d13931346d5c04d8c85d99d5cb8d60
trujilml <mickeytrj@gmail.com>
2022-02-25 13:32:16 -0600

    deployment

---
+++ client/build/precache-manifest.7375dbc5ac85ae36a5d62e84a06b4878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac85354d3cf38e328123514a2f26490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc00ca943867f390aab",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "ad241284b74c6fdfdc46",
+    "url": "/static/js/2.c46a6261.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.c46a6261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc00ca943867f390aab",
+    "url": "/static/js/main.9ff0720d.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
